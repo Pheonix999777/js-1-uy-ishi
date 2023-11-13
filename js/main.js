c7954aa8af55fe2918f6d8f635b445445f27b1b8
Pheonix999777 <Pheonix4777@gmail.com>
2023-11-13 23:39:29 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,97 @@
+let users = [
+  {
+    name: "Nurmuhammad",
+    age: 22,
+    position: "mentor",
+    gender: "male",
+  },
+
+  {
+    name: "Sardor",
+    age: 20,
+    position: "asistent",
+    gender: "male",
+  },
+
+  {
+    name: "Muhammadrasul",
+    age: 18,
+    position: "o'quvchi",
+    gender: "male",
+  },
+
+  {
+    name: "Ohunjon",
+    age: 20,
+    position: "o'quvchi",
+    gender: "male",
+  },
+
+  {
+    name: "Yasharbek",
+    age: 20,
+    position: "o'quvchi",
+    gender: "male",
+  },
+
+  {
+    name: "Hasanali",
+    age: 21,
+    position: "o'quvchi",
+    gender: "male",
+  },
+
+  {
+    name: "Usmon",
+    age: 18,
+    position: "o'quvchi",
+    gender: "male",
+  },
+
+  {
+    name: "Salohiddin",
+    age: 18,
+    position: "o'quvchi",
+    gender: "male",
+  },
+];
+
+let userNames = document.querySelectorAll(".names");
+userNames[0].textContent = users[0].name;
+userNames[1].textContent = users[1].name;
+userNames[2].textContent = users[2].name;
+userNames[3].textContent = users[3].name;
+userNames[4].textContent = users[4].name;
+userNames[5].textContent = users[5].name;
+userNames[6].textContent = users[6].name;
+userNames[7].textContent = users[7].name;
+
+let ages = document.querySelectorAll(".age");
+ages[0].textContent = users[0].age;
+ages[1].textContent = users[1].age;
+ages[2].textContent = users[2].age;
+ages[3].textContent = users[3].age;
+ages[4].textContent = users[4].age;
+ages[5].textContent = users[5].age;
+ages[6].textContent = users[6].age;
+ages[7].textContent = users[7].age;
+
+let positions = document.querySelectorAll(".position");
+positions[0].textContent = users[0].position;
+positions[1].textContent = users[1].position;
+positions[2].textContent = users[2].position;
+positions[3].textContent = users[3].position;
+positions[4].textContent = users[4].position;
+positions[5].textContent = users[5].position;
+positions[6].textContent = users[6].position;
+positions[7].textContent = users[7].position;
+
+let genders = document.querySelectorAll(".gender");
+genders[0].textContent = users[0].gender;
+genders[1].textContent = users[1].gender;
+genders[2].textContent = users[2].gender;
+genders[3].textContent = users[3].gender;
+genders[4].textContent = users[4].gender;
+genders[5].textContent = users[5].gender;
+genders[6].textContent = users[6].gender;
+genders[7].textContent = users[7].gender;
